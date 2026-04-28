c4ea38a9fe0d81e7b140acfc2dc680ebe5cea8dc
6ot9lpa <sp1vaks@yandex.ru>
2026-04-28 19:29:36 +0300

    test: add smoke logging helper

--- tests/test_framework_smoke.py
+++ tests/test_framework_smoke.py
@@ -1,16 +1,22 @@
 from framework.core.settings import ConfigManager, TestDataManager as TestData
 from framework.elements import Button, ElementFactory, Input, TextElement
 from framework.models.config import LoggingConfig
+from tests.utils import LogClass, log
 
 
 def test_framework_smoke_imports_and_config() -> None:
+    log(LogClass.INFO, "Test started: Smoke Framework")
+
     config = ConfigManager().config
 
     assert config.base_url
     assert config.explicit_wait >= 0
     assert isinstance(TestData().data, dict)
     assert LoggingConfig.from_dict({"console_enabled": False}).console_enabled is False
+    assert LogClass.INFO.value == "INFO"
     assert Button.__name__ == "Button"
     assert ElementFactory.__name__ == "ElementFactory"
     assert Input.__name__ == "Input"
     assert TextElement.__name__ == "TextElement"
+
+    log(LogClass.INFO, "Test finished: Smoke Framework")
---
+++ tests/utils/__init__.py
@@ -0,0 +1,6 @@
+from tests.utils.logging_utils import LogClass, log
+
+__all__ = [
+    "LogClass",
+    "log",
+]
